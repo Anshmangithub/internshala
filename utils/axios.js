9b0ba236cff39fe930c0db4074af0eae0c75dfd3
Ansh gajbhiye <111952775+Anshmangithub@users.noreply.github.com>
2024-08-30 00:02:46 +0530

    Update axios.js

--- utils/axios.js
+++ utils/axios.js
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL : "http://localhost:8080/",
+    baseURL : "https://talent-forge-backend-sigma.vercel.app/",
     withCredentials : true
 })
 
